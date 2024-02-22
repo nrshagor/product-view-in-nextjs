69292e5b614e5923673c07f131fcba297db86fb2
nrshagor <noorerabbishagor@gmail.com>
2024-02-23 04:08:01 +0600

    product details

--- src/app/ProductsData/[...productId]/page.js
+++ src/app/ProductsData/[...productId]/page.js
@@ -2,8 +2,9 @@
 
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "./details.scss";
 function Page({ params }) {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(null);
 
   useEffect(() => {
     fetchData();
@@ -19,16 +20,72 @@ function Page({ params }) {
       console.error("Error fetching data:", error);
     }
   };
-  console.log(data);
+
   return (
-    <div>
-      <img src={data.pictures} alt={data.title} className="product-image" />
-      <div className="product-info">
-        <p className="product-title">Title: {data.title}</p>
-        <p className="product-text">Price: {data.price}</p>
-        <p className="product-text">Status: {data.status}</p>
-        <p className="product-text">Target Price 1: {data.targetPrice1}</p>
-      </div>
+    <div className="details-setion">
+      {data && (
+        <div className="details-container">
+          <h1>Product Details</h1>
+          <div className="product-info">
+            <div className="left">
+              <img
+                src={data.product.shippableProduct.pictures.list[0]}
+                alt={data.product.shippableProduct.title}
+                className="product-image"
+              />
+            </div>
+            <div className="right">
+              <p>ID: {data.id}</p>
+              <p>Title: {data.product.shippableProduct.title}</p>
+              <p>Brand: {data.product.shippableProduct.brandName}</p>
+              <p>Product Type: {data.product.shippableProduct.productType}</p>
+              <p>Category: {data.product.shippableProduct.category}</p>
+              <p>Subcategory: {data.product.shippableProduct.subcategory}</p>
+              <p>Subcategory 2: {data.product.shippableProduct.subcategory2}</p>
+              <p>Subcategory 3: {data.product.shippableProduct.subcategory3}</p>
+              <p>Description 1: {data.product.shippableProduct.description1}</p>
+              <p>Description 2: {data.product.shippableProduct.description2}</p>
+              <p>
+                Manufacturer SKU:{" "}
+                {data.product.shippableProduct.manufacturerSku}
+              </p>
+              <p>Seller SKU: {data.product.shippableProduct.sellerSku}</p>
+              <p>UPC: {data.product.shippableProduct.upc}</p>
+              <p>
+                Specifications:{" "}
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: data.product.shippableProduct.specifications,
+                  }}
+                />
+              </p>
+              <p>
+                Additional Notes:{" "}
+                {data.product.shippableProduct.additionalNotes}
+              </p>
+              <p>
+                Is Refurbished: {data.product.shippableProduct.isRefurbished}
+              </p>
+              <p>Notes 2: {data.product.shippableProduct.notes2}</p>
+              <p>MSRP: {data.product.shippableProduct.msrp}</p>
+              <p>Warranty: {data.product.shippableProduct.warranty}</p>
+              <p>
+                Manufacturer Country:{" "}
+                {data.product.shippableProduct.manufacturerCountry}
+              </p>
+              <p>Price: {data.product.shippableProduct.price}</p>
+              <p>In Stock: {data.inStock}</p>
+              <p>Target Price 1: {data.targetPrice1}</p>
+              <p>Target Price 2: {data.targetPrice2}</p>
+              <p>Target Price 3: {data.targetPrice3}</p>
+              <p>Max Sale Limit: {data.maxSaleLimit}</p>
+              <p>Campaign ID: {data.campaign.id}</p>
+              <p>Campaign Name: {data.campaign.name}</p>
+              <p>Campaign End Date: {data.campaign.endDate}</p>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
